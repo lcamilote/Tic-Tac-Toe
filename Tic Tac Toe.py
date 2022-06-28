934c9263e958f5ad6c3d2d4d405b628ccfd9a72d
lcamilote <107189672+lcamilote@users.noreply.github.com>
2022-06-28 10:33:52 -0300

    tic tac toe 1.1v
    changed the order of the board position.

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,6 +1,7 @@
+# To print the board
 def display_board(board):
     print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('   |   |')
     print('-----------')
     print('   |   |')
@@ -8,30 +9,31 @@ def display_board(board):
     print('   |   |')
     print('-----------')
     print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
     print('   |   |')
 
-
+# To choose the marker (X or O)
 def player_input():
     marker = ''
 
     while not (marker == 'X' or marker == 'O'):
-        marker = input('Player 1: Do you want to be X or O? ').upper()
+        marker = input('Player 1: Do you want to be X or O?: ').upper()
 
     if marker == 'X':
         return ('X', 'O')
     else:
         return ('O', 'X')
 
-
+# To place the marker (X or O) in the board position (1-9).
 def place_marker(board, marker, position):
     board[position] = marker
 
 
+# to check if the marker (X or O) Won, across the board.
 def win_check(board, mark):
-    return ((board[7] == mark and board[8] == mark and board[9] == mark) or  # across the top
+    return ((board[1] == mark and board[2] == mark and board[3] == mark) or  # across the top
             (board[4] == mark and board[5] == mark and board[6] == mark) or  # across the middle
-            (board[1] == mark and board[2] == mark and board[3] == mark) or  # across the bottom
+            (board[7] == mark and board[8] == mark and board[9] == mark) or  # across the bottom
             (board[7] == mark and board[4] == mark and board[1] == mark) or  # down the middle
             (board[8] == mark and board[5] == mark and board[2] == mark) or  # down the middle
             (board[9] == mark and board[6] == mark and board[3] == mark) or  # down the right side
@@ -41,25 +43,25 @@ def win_check(board, mark):
 
 import random
 
-
+# basically to flip a coin - head or tails
 def choose_first():
     if random.randint(0, 1) == 0:
         return 'Player 2'
     else:
         return 'Player 1'
 
-
+# To check if the position it is free ' '
 def space_check(board, position):
     return board[position] == ' '
 
-
+# To check if the board is full from the position 1 to 9.
 def full_board_check(board):
     for i in range(1, 10):
         if space_check(board, i):
             return False
     return True
 
-
+# The player to choose the position, and it is being checked against the space_check function
 def player_choice(board):
     position = 0
 
@@ -91,7 +93,7 @@ while True:
     turn = choose_first()
     print(turn + ' will go first.')
 
-    play_game = input('Are you ready to play? Enter Yes or No.')
+    play_game = input('Are you ready to play? Enter Yes or No: ')
 
     if play_game.lower()[0] == 'y':
         game_on = True
